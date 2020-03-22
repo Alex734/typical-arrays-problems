0cd591aee42ef7225e9c63a4f4f0c67bd6e027fd
Aliaksandr Huzau <huzau.aliaksandr@gmail.com>
2020-03-22 12:03:24 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array.length == 0 || array == null || array == undefined) {
+    return 0;
+  }
+  else{
+    let min_value = array[0];
+  array.forEach(arr_elem => {
+    if(min_value > arr_elem){
+      min_value = arr_elem;
+    } 
+  });
+  return min_value;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array.length == 0){
+    return 0;
+  }
+  let max_value = array[0];
+  array.forEach(arr_elem => {
+    if(max_value < arr_elem)
+      max_value = arr_elem; 
+   });
+  return max_value; 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(arr_elem => {
+    sum += arr_elem; 
+  });
+  let avg_value = sum / array.length;
+  return avg_value; 
 }
